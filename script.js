e6a0e0394620b7505df254873858ea0afdbb44a3
Ona Narbutas <ona.narbutas@gmail.com>
2022-07-08 16:24:12 -0400

    Rewrote grid creation as functions to respond to user input, but now styles aren't being applied.

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 const container = document.querySelector('#container');
+let userInput = document.querySelector('button');
+const boxes = document.querySelectorAll('.boxes');
 
 //loop to create divs that will comprise 16x16 grid, add class "boxes" and 
 //individual ID (tie to i), and append to container
-for (let i = 0; i < 256; i++) {
-    const newDiv = document.createElement('div');
-    newDiv.setAttribute('class', 'boxes');
-    newDiv.setAttribute('id', `box${i+1}`);
-    container.appendChild(newDiv);
+
+function createBoxes(res) {
+    for (let i = 0; i < (res * res); i++) {
+        const newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'boxes');
+        container.appendChild(newDiv);
+
+        boxes.forEach((box) => {
+            box.style.backgroundColor = 'lightgray';
+            box.style.height = 'auto';
+            box.style.width = 'auto';
+
+            //turn box from lightgray to gray on hover
+            box.addEventListener('mouseenter', () =>{
+                box.style.backgroundColor = 'gray';
+            });
+        });
+    }
 }
 
-
-//dictate size of boxes
-const boxes = document.querySelectorAll('.boxes');
-boxes.forEach((box) => {
-    box.style.backgroundColor = 'lightgray';
+createBoxes(16);
 
 
-    //turn box from lightgray to gray on hover
-    box.addEventListener('mouseenter', () =>{
-        box.style.backgroundColor = 'gray';
-        box.style.width = 'auto';
-    });
+
+//get number of pixels from user and adjust grid accordingly
+userInput.addEventListener('click', () => {
+    container.style.gridTemplateColumns = `${getAutos(userInput)}`;
+    container.style.gridTemplateRows = `${getAutos(userInput)}`;
+
+    boxes.forEach(box, () => {
+        container.removeChild(box);
+    })
+    createBoxes(userInput);
 })
+
+//convert user input into correct number of "autos" necessary to 
+//control gridTemplateColumns and gridTemplateRows;
+function getAutos(input) {
+    let output = '';
+    for (let i = 0; i < (input); i++) {
+        output += 'auto ';
+    }
+    return output;
+}
